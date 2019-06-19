8be66ddfb24a966eea3e431f1bd4404925c45469
lkg <627861816@qq.com>
2019-06-19 15:02:31 +0800

    scrapy demo Itcast

---
+++ scrapy/mySpider/spiders/itcast.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class ItcastSpider(scrapy.Spider):
+    name = 'itcast'
+    allowed_domains = ['itcast.cn']
+    start_urls = ['http://www.itcast.cn/channel/teacher.shtml']
+
+    def parse(self, response):
+        # 存放老师信息的集合
+        items = []
+
+        for each in response.xpath("//div[@class='li_txt']"):
+            # 将我们得到的数据封装到一个 `ItcastItem` 对象
+            from ..items import MyspiderItem
+            item = MyspiderItem()
+            # extract()方法返回的都是unicode字符串
+            name = each.xpath("h3/text()").extract()
+            title = each.xpath("h4/text()").extract()
+            info = each.xpath("p/text()").extract()
+
+            # xpath返回的是包含一个元素的列表
+            item['name'] = name[0]
+            item['title'] = title[0]
+            item['info'] = info[0]
+
+            items.append(item)
+
+        # 直接返回最后数据
+        return items
+
+        # # 获取网站标题
+        # context = response.xpath('/html/head/title/text()')
+        # # 提取网站标题
+        # title = context.extract_first()
+        # print(title)
+
+        # filename = "teacher.html"
+        # open(filename, 'w', encoding='utf-8').write(response.body.decode(encoding='utf-8'))
