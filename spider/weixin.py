26c406d5ff723609a00e96a3eb999470f3572d4c
lkg <627861816@qq.com>
2019-06-26 14:15:07 +0800

    利用selenium 爬取微信公众号

--- spider/weixin.py
+++ spider/weixin.py
@@ -1,7 +1,5 @@
-import requests
-from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 import time
 
 headers = {
@@ -19,11 +17,33 @@ headers = {
 url = "https://weixin.sogou.com/weixin"
 
 if __name__ == '__main__':
-    query_url = "https://weixin.sogou.com/weixin?type=1&query={}&ie=utf8&s_from=input&_sug_=y&_sug_type_=".format("石油")
-
     driver = webdriver.Chrome('C:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver')
-    driver.get(query_url)
-
+    query_url = "https://weixin.sogou.com/weixin?type=1&query={}&ie=utf8&s_from=input&_sug_=y&_sug_type_=".format("石油")
+    driver.get(url)
+    driver.find_element_by_id("query").send_keys("python")
+    driver.find_element_by_xpath("//input[@value='搜公众号']").click()
+    element = driver.find_element_by_xpath("//a[@uigs='account_name_1']").click()
+    base_handle = driver.current_window_handle
+    for handle in driver.window_handles:
+        if handle != base_handle:
+            driver.switch_to.window(handle)
+            elements = driver.find_elements_by_class_name("weui_media_bd")
+            num = len(elements)
+            for i in range(len(elements)):
+                bd = driver.find_elements_by_class_name("weui_media_bd")
+                publish_time = bd[i].find_element_by_class_name("weui_media_extra_info").text
+                elements = driver.find_elements_by_class_name("weui_media_title")
+                print("进入公众号文章页面")
+                elements[i].click()
+                try:
+                    title = driver.find_element_by_class_name("rich_media_title").text
+                    content = driver.find_element_by_id("js_content").text
+                except NoSuchElementException as e:
+                    print()
+                time.sleep(3)
+                driver.back()
+            driver.close()
+    driver.switch_to.window(base_handle)
     time.sleep(3)
     driver.close()
 
