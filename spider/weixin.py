8a871880f5a8a50ad6b3f2abf6a793b350a2d0d5
lkgGitHub <627861816@qq.com>
2019-07-09 00:40:41 +0800

    加代理

--- spider/weixin.py
+++ spider/weixin.py
@@ -21,18 +21,27 @@ url = "https://weixin.sogou.com/weixin"
 
 
 if __name__ == '__main__':
-    driver = webdriver.Chrome('/Users/mac/software/chromedriver')
+    # 设置代理
+    PROXY = "61.168.162.32:80"
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--proxy-server=http://124.232.133.199:3128")
+
+    driver = webdriver.Chrome('/Users/mac/software/chromedriver', options=chrome_options)
     driver.get(url)
     time.sleep(random.uniform(0, 2))
-
+    i = 1
     with open("filename.csv", "r") as csvfile:
         reader = csv.reader(csvfile)
         for r in reader:
-            print(r)
+            print(str(i) + ":" + str(r))
+            i = i + 1
             # driver = webdriver.Chrome('C:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver')
             # query_url = "https://weixin.sogou.com/weixin?type=1&query={}&ie=utf8&s_from=input&_sug_=y&_sug_type_=".format("石油")
-            driver.find_element_by_id("query").clear()
-            driver.find_element_by_id("query").send_keys(r[1])
+            try:
+                driver.find_element_by_id("query").clear()
+                driver.find_element_by_id("query").send_keys(r[1])
+            except NoSuchElementException as e:
+                print("更换代理")
             time.sleep(random.uniform(1, 3))
             driver.find_element_by_xpath("//input[@value='搜公众号']").click()
             time.sleep(random.uniform(3, 4))
@@ -49,20 +58,22 @@ if __name__ == '__main__':
                     num = len(elements)
                     for i in range(3):
                         bd = driver.find_elements_by_class_name("weui_media_bd")
-                        publish_time = bd[i].find_element_by_class_name("weui_media_extra_info").text
-                        elements = driver.find_elements_by_class_name("weui_media_title")
-                        print("进入公众号文章页面")
-                        elements[i].click()
                         try:
+                            publish_time = bd[i].find_element_by_class_name("weui_media_extra_info").text
+                            elements = driver.find_elements_by_class_name("weui_media_title")
+                            elements[i].click()
                             title = driver.find_element_by_class_name("rich_media_title").text
                             content = driver.find_element_by_id("js_content").text
                             print("title:", title)
-                        except NoSuchElementException as e:
-                            print()
+                        except (NoSuchElementException, IndexError) as e:
+                            print("更换代理")
+                            break
                         time.sleep(random.uniform(1, 3))
                         driver.back()
+                    driver.quit()
                     driver.close()
             driver.switch_to.window(base_handle)
             time.sleep(random.uniform(1, 2))
+    driver.quit()
     driver.close()
 
