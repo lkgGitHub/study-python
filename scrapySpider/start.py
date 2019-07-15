67671cbace88b1933b58cd42ff7e6cb7b8698207
lkg <627861816@qq.com>
2019-07-15 14:10:54 +0800

    第一财经爬虫

--- scrapySpider/start.py
+++ scrapySpider/start.py
@@ -1,4 +1,5 @@
 from scrapy import cmdline
 
-cmdline.execute("scrapy crawl ftchinese -o news.csv".split())
+cmdline.execute("scrapy crawl yicai -o news.csv".split())
+# cmdline.execute("scrapy crawl ftchinese -o news.csv".split())
 # cmdline.execute("scrapy crawl sogou_weixin0 -o filename.csv".split())
