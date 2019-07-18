cec45f1f8809eb35d476dbe9f94031f446b5caff
lkg <627861816@qq.com>
2019-07-18 15:37:01 +0800

    在两个请求之间传递参数，实现第一财经完全scrapy化

--- scrapySpider/scrapySpider/spiders/yicai.py
+++ scrapySpider/scrapySpider/spiders/yicai.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 import json
+import random
+import time
 
-import requests
 import scrapy
-from bs4 import BeautifulSoup
+
 from scrapy import Request
 
 from ..items import NewsItem
@@ -34,7 +35,7 @@ class YicaiSpider(scrapy.Spider):
 
     def start_requests(self):
         total_page = 1
-        page_size = 25
+        page_size = 5
         for page in range(1, total_page + 1):
             yield Request(url="https://www.yicai.com/api/ajax/getlatest?page={}&pagesize={}".format(page, page_size),
                           headers=headers, dont_filter=True)  # dont_filter=True 为不过滤url
@@ -45,42 +46,55 @@ class YicaiSpider(scrapy.Spider):
                             'application/signed-exchange;v=b3 '
         news_list = json.loads(response.text)
         for news in news_list:
-            news_item = NewsItem()
-            channel_name = news.get("ChannelName")
-            news_item['public_time'] = news.get("pubDate")
-            news_item['author'] = news.get("NewsAuthor") + " " + news.get("CreaterName")
-            news_source = news.get("NewsSource")
-            news_item['summary'] = news.get("NewsNotes")
-            news_item['title'] = news.get("NewsTitle")
             url = news.get("url")
             if "news" in url:
                 news_url = base_url + url
-                new_response = requests.get(news_url, headers=headers)
-                news_soup = BeautifulSoup(new_response.text, 'lxml')
-                keywords = news_soup.find("meta", attrs={"name": "keywords"})["content"]
-                news_txt = news_soup.find("div", class_="m-txt")
-                content = ""
-                for p in news_txt.findAll('p'):
-                    try:
-                        content = content + p.string + "\r\n"
-                    except TypeError:
-                        pass
-                print("keywords: %s, content: %s" % (keywords, content))
-                news_item['content'] = content
-                news_item["remark"] = {"频道": channel_name, "来源": news_source, "关键字": keywords}
+                print(news_url)
+                yield Request(url=news_url, headers=headers, callback=self.parse_item,
+                              meta={
+                                  "NewsTitle": news.get("NewsTitle"),
+                                  "author": news.get("NewsAuthor") + " " + news.get("CreaterName"),
+                                  "pubDate": news.get("pubDate"),
+                                  "NewsNotes": news.get("NewsNotes"),
+                                  "remark": "频道:{};来源:{};".format(news.get("ChannelName"), news.get("NewsSource"))
+                              })  # meta={}：请求之间传递参数，即在Request 之间传递参数
             elif "video" in url:
                 print(url)
             else:
                 print(url)
-            yield news_item
 
+    @staticmethod
+    def parse_item(response):
+        time.sleep(random.uniform(1, 2))
+        news_item = NewsItem()
+        news_item['title'] = response.meta["NewsTitle"]
+        news_item['public_time'] = response.meta["pubDate"]
+        news_item['author'] = response.meta["author"]
+        news_item['summary'] = response.meta["NewsNotes"]
+        keywords = response.xpath("//meta[@name='keywords']/@content").extract_first()
+        news_item["remark"] = response.meta["remark"] + "关键字:{}".format(keywords)
+        response.xpath("//div[@class_='m-txt']")
+        contents = response.xpath("//div[@class='m-txt']/p/text()")
+        content = ""
+        for c in contents:
+            content = content + c.extract()
+        news_item["content"] = content
+        yield news_item
+
+    # import requests
+    # from bs4 import BeautifulSoup
     # def parse_news(response):
-    #     new_keywords = response.xpath("//meta[@name='keywords']/@content").extract_first()
-    #     response.xpath("//div[@class_='m-txt']")
-    #     contents = response.xpath("//div[@class='m-txt']/p/text()")
+    #     new_response = requests.get(news_url, headers=headers)
+    #     news_soup = BeautifulSoup(new_response.text, 'lxml')
+    #     keywords = news_soup.find("meta", attrs={"name": "keywords"})["content"]
+    #     news_txt = news_soup.find("div", class_="m-txt")
     #     content = ""
-    #     for c in contents:
-    #         content = content + c.extract()
-    #     print("keywords:", new_keywords)
-    #     print("content:", content)
+    #     for p in news_txt.findAll('p'):
+    #         try:
+    #             content = content + p.string + "\r\n"
+    #         except TypeError:
+    #             pass
+    #     print("keywords: %s, content: %s" % (keywords, content))
+    #     news_item['content'] = content
+    #     news_item["remark"] = {"频道": channel_name, "来源": news_source, "关键字": keywords}
 
