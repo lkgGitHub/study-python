0de5438e44395aea84a4bbeb2e4b155f475987b0
lkg <627861816@qq.com>
2019-12-16 12:10:05 +0800

    百度元搜索和盘搜搜索爬虫

--- sgcc/sgcc.py
+++ sgcc/sgcc.py
@@ -2,5 +2,7 @@ from scrapy import cmdline
 
 # -o weixin.csv
 # cmdline.execute("scrapy crawl sogou".split())
-cmdline.execute("scrapy crawl wenku".split())
+# cmdline.execute("scrapy crawl wenku".split())
+# cmdline.execute("scrapy crawl baidu".split())
+cmdline.execute("scrapy crawl panSou".split())
 
--- sgcc/sgcc/spiders/baidu.py
+++ sgcc/sgcc/spiders/baidu.py
@@ -3,20 +3,20 @@ import scrapy
 from scrapy import Request
 
 
-headers = {
-    'Accept': '*/*',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Accept-Language': 'zh-CN,zh;q=0.9',
-    'Connection': 'keep-alive',
-    'Host': 'www.baidu.com',
-    'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Site': 'same-origin',
-    'Cache-Control': 'max-age=0',
-    'Upgrade-Insecure-Requests': '1',
-    'Referer': 'http://www.baidu.com/',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                  'Chrome/78.0.3904.108 Safari/537.36',
-    'X-Requested-With': 'XMLHttpRequest'
+base_headers = {
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1",
+    "Sec-Fetch-User": "?1",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-Mode": "navigate",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "zh-CN,zh;q=0.9",
+    "Host": "www.baidu.com",
+    "is_referer": "https://www.baidu.com/",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/78.0.3904.108 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,"
+              "application/signed-exchange;v=b3",
 }
 
 
@@ -28,8 +28,14 @@ class BaiduSpider(scrapy.Spider):
     def start_requests(self):
         keyword = '国网'
         url = "https://www.baidu.com/s?ie=utf-8&wd={}".format(keyword)
-        yield Request(url, headers=headers, callback=self.parse)
+        search_url = "https://www.baidu.com/s?ie=utf-8&wd={wd}"
+        yield Request(search_url.format(wd=keyword), headers=base_headers, callback=self.parse)
 
     def parse(self, response):
-        print(response.text)
-
+        div = response.xpath("//div[@id='content_left']/div[@class='result c-container ']")
+        print("len:", len(div))
+        for d in div:
+            title = d.xpath("./h3/a/text()").extract_first()
+            href = d.xpath("./h3/a/@href").extract_first()
+            print(title)
+            print(href)
