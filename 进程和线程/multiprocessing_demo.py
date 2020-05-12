ea60bfdd893afd254249bc96e679a6cdda7606e2
lkg <627861816@qq.com>
2020-05-12 14:05:06 +0800

    url project

--- 进程和线程/multiprocessing_demo.py
+++ 进程和线程/multiprocessing_demo.py
@@ -38,8 +38,10 @@ def process_pool():
 
 
 if __name__ == '__main__':
-    print('$ nslookup')
-    p = subprocess.Popen(['nslookup'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output, err = p.communicate(b'set q=mx\npython.org\nexit\n')
-    print(output.decode('utf-8'))
-    print('Exit code:', p.returncode)
+    process_pool()
+
+    # print('$ nslookup')
+    # p = subprocess.Popen(['nslookup'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # output, err = p.communicate(b'set q=mx\npython.org\nexit\n')
+    # print(output.decode('utf-8'))
+    # print('Exit code:', p.returncode)
