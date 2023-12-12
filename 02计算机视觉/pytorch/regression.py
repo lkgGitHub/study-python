faac907fdec510388c749537c848d8c4511ec44d
lkg <kaigangli@deepglint.com>
2023-12-12 14:20:06 +0800

    2

--- 02计算机视觉/pytorch/regression.py
+++ 02计算机视觉/pytorch/regression.py
@@ -76,13 +76,13 @@ if __name__ == '__main__':
 
     print("input_features shape:", input_features.shape)  # (348, 14)
     # 权重参数初始化
-    weights = torch.randn((14, 128), dtype=float, requires_grad=True)
-    biases = torch.randn((128), dtype=float, requires_grad=True)
-    weights2 = torch.randn((128, 1), dtype=float, requires_grad=True)
-    biases2 = torch.randn(1, dtype=float, requires_grad=True)
-
-    learning_rate = 0.01
-    losses = []
+    # weights = torch.randn((14, 128), dtype=float, requires_grad=True)
+    # biases = torch.randn((128), dtype=float, requires_grad=True)
+    # weights2 = torch.randn((128, 1), dtype=float, requires_grad=True)
+    # biases2 = torch.randn(1, dtype=float, requires_grad=True)
+    #
+    # learning_rate = 0.01
+    # losses = []
     # for i in range(1000):
     #     # 计算隐层。mm (matrix multiplication)，即矩阵乘法
     #     hidden = x.mm(weights) + biases
@@ -109,5 +109,37 @@ if __name__ == '__main__':
     #     weights2.grad.data.zero_()
     #     biases2.grad.data.zero_()
 
-        # 更简单的构建网络模型
-        input_size = input_features.shape[1]
+    # 更简单的构建网络模型
+    input_size = input_features.shape[1]
+    hidden_size = 128
+    output_size = 1
+    batch_size = 16
+    # Sequential 连续的
+    # torch.nn.Sequential 是一个容器，可以将多个层组合成一个模型
+    my_nn = torch.nn.Sequential(
+        torch.nn.Linear(input_size, hidden_size),
+        torch.nn.Sigmoid(),  # 激活函数
+        torch.nn.Linear(hidden_size, output_size)
+    )
+    cost = torch.nn.MSELoss(reduction="mean")
+    optimizer = torch.optim.Adam(my_nn.parameters(), lr=0.01)
+
+    # 训练网络
+    losses = []
+    for i in range(1000):
+        batch_loss = []
+        # MINI-Batch方法来进行训练
+        for start in range(0, len(input_features), batch_size):
+            end = start + batch_size if start + batch_size < len(input_features) else len(input_features)
+            xx = torch.tensor(input_features[start:end], dtype=torch.float, requires_grad=True)
+            yy = torch.tensor(labels[start: end], dtype=torch.float, requires_grad=True)
+            prediction = my_nn(xx)
+            loss = cost(prediction, yy)
+            optimizer.zero_grad()
+            loss.backward(retain_graph=True)
+            optimizer.step()
+            batch_loss.append(loss.data.numpy())
+        # 打印损失
+        if i % 100 == 0:
+            losses.append(np.mean(batch_loss))
+            print(i, np.mean(batch_loss))
