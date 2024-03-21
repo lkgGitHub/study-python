232393dda1a642ef0885e6d51f23419b3d64dd3e
lkg <627861816@qq.com>
2024-03-21 09:45:18 +0800

    to

--- 00/utils/convertor.py
+++ 00/utils/convertor.py
@@ -1,6 +1,9 @@
 import json
+import os
+import shutil
+from os import path
 
-original_data = ""
+import cv2
 
 
 def read_json_file(filename):
@@ -25,10 +28,49 @@ def read_json_file(filename):
     return json_objects
 
 
+def cv_show(img, name):
+    cv2.imshow(name, img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def absolute_to_relative(img, absolute_coord):
+    """
+    将图片的相对坐标转换为绝对坐标。
+
+    Args:
+      img: 输入图像。
+      absolute_coord: 相对坐标，格式为 (x, y)。
+
+    Returns:
+      绝对坐标，格式为 (x, y)。
+    """
+
+    img_h, img_w = img.shape[:2]
+    x = float(absolute_coord[0] / img_w)
+    y = float(absolute_coord[1] / img_h)
+    return x, y
+
+
 if __name__ == '__main__':
     lines2 = read_json_file(original_data)
     for line in lines2:
         results = line["result"]["result"]
         for result in results:
             if len(result['data']) > 0:
-                print(line["result"]['image'])
+                img = cv2.imread(path.join(image_base_path, line["result"]['image']))
+                # cv2.rectangle(img, (int(result['data'][0]), int(result['data'][1])),
+                #               (int(result['data'][2]), int(result['data'][3])),
+                #               (255, 0, 0), 3)
+                # cv_show(img, line["result"]['image'])
+                filename = os.path.basename(line["result"]['image'])
+                src_path = path.join(image_base_path, line["result"]['image'])
+                dst_path = path.join(dst_dir, "images", filename)
+                shutil.copy(src_path, dst_path)
+
+                x1, y1 = absolute_to_relative(img, (int(result['data'][0]), int(result['data'][1])))
+                x2, y2 = absolute_to_relative(img, (int(result['data'][2]), int(result['data'][3])))
+                fn, ext = os.path.splitext(filename)
+                label_path = path.join(dst_dir, "labels", fn + ".txt")
+                with open(label_path, "w") as f:
+                    f.write(f"1 {x1} {y1} {x2} {y2} \n")
